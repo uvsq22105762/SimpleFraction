435959e160146068b94de54ba8fb6d03537eac55
uvsq22105762 <zachariebaril@gmail.com>
2021-09-30 14:17:03 +0200

    zdki

---
+++ CP_TD1/src/TD1/Main.java
@@ -0,0 +1,10 @@
+package TD1;
+
+public class Main {
+	public static void main(String[] args) {
+		Fraction fraction = new Fraction(14, 7);
+		System.out.println(fraction.arePrimeBetweenThem());
+		System.out.println(fraction.value());
+		System.out.println(fraction.toString());
+	}
+}
