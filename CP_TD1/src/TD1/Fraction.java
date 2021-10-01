af78cf7ca447545d527757369d18ee8f0bb0af03
uvsq22105762 <zachariebaril@gmail.com>
2021-10-01 13:25:57 +0200

    zdki

--- CP_TD1/src/TD1/Fraction.java
+++ CP_TD1/src/TD1/Fraction.java
@@ -4,29 +4,28 @@ public class Fraction {
 
 	private int numerateur;
 	private int denominateur;
-	
+
 	public Fraction(int numerateur, int denominateur) {
 		super();
 		this.numerateur = numerateur;
 		this.denominateur = denominateur;
 	}
-	
+
 	public float value() {
-		if(denominateur != 0) {
+		if (denominateur != 0) {
 			return numerateur / denominateur;
 		}
 		return 0;
 	}
-	
+
 	public boolean arePrimeBetweenThem() {
 		boolean primeBetweenThem = true;
-		if(numerateur % denominateur == 0) {
+		if (numerateur % denominateur == 0) {
 			primeBetweenThem = false;
 		}
-		if(denominateur % numerateur == 0) {
+		if (denominateur % numerateur == 0) {
 			primeBetweenThem = false;
 		}
-		
 		return primeBetweenThem;
 	}
 
